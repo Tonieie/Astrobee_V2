acdff5bcdfdf653d8d5bb6910dde73c6b07352c9
Tonieie <tonzazil12@gmail.com>
2021-06-19 18:11:36 +0700

    added alignX, relativeMoveToWrapper methods

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -95,40 +95,46 @@ public class YourService extends KiboRpcService {
         Point3 QR_target = new Point3(11.21f, -9.8f, 4.79);
         moveToWrapper( QR_target.x,QR_target.y,QR_target.z,0,0,-0.707,0.707);
         Log.d("QR","Start to read QR");
-        // Mat imageCamera = api.getMatNavCam();
 
-        Mat imageCamera = croppedImage();
-        String qr_str = decodeQR(imageCamera);
-        StringDecode QRData = new StringDecode();
-        QRData.setString(qr_str);
-        Log.d("QR","End to read QR");
-        Log.d("QR", String.format("QR A : %d %.2f %.2f %.2f",QRData.getPattern(),QRData.getPosX(),QRData.getPosY(),QRData.getPosZ()));
-
-        moveToWrapper( QRData.getPosX(),QRData.getPosY(),QRData.getPosZ(),0,0,-0.707,0.707); // go to A'
-        Log.d("AR","Moved to A'");
-        Mat ar_pic = api.getMatNavCam();
+        Mat imageCamera = api.getMatNavCam();
         Kinematics kinec_takepic = api.getTrustedRobotKinematics();
         Point pos_takepic = kinec_takepic.getPosition();
 
-        //ARUCO
+        zbarQR_obj = new zbarQR();
+        zbarQR_obj.scanQRImage(imageCamera);
+        Log.d("QR","readed : " + zbarQR_obj.qrCodeString);
+        api.sendDiscoveredQR(zbarQR_obj.qrCodeString);      //scan QR code
+
+        StringDecode QRData = new StringDecode();
+        QRData.setString(zbarQR_obj.qrCodeString);      //split decoded QR code
+        Log.d("QR","End to read QR");
+        Log.d("QR", String.format("QR A : %d %.2f %.2f %.2f",QRData.getPattern(),QRData.getPosX(),QRData.getPosY(),QRData.getPosZ()));
+
+        //AR
+//        moveToWrapper( QRData.getPosX(),QRData.getPosY(),QRData.getPosZ(),0,0,-0.707,0.707); // go to A'
+//        Log.d("AR","Moved to A'");
+//        Mat ar_pic = api.getMatNavCam();
+//        Kinematics kinec_takepic = api.getTrustedRobotKinematics();
+//        Point pos_takepic = kinec_takepic.getPosition();
+
+
         ARmodel ArucoModel = new ARmodel();
-//        ArucoModel.estimate(imgProc.processedImg,camMatrix,dstMatrix);
-        ArucoModel.estimate(ar_pic,camMatrix,dstMatrix);
-//        Point3 AR_target = new Point3( QR_target.x + ArucoModel.getPosX(),QR_target.y + ArucoModel.getPosY() ,QR_target.z + ArucoModel.getPosZ());
+        ArucoModel.estimate(imageCamera,camMatrix,dstMatrix);
         Log.d("AR", String.format("AR relative : %.2f %.2f %.2f",ArucoModel.getPosX(),ArucoModel.getPosY(),ArucoModel.getPosZ()));
-//        Log.d("QR", String.format("AR absolute : %.2f %.2f %.2f",AR_target.x,AR_target.y,AR_target.z));
 
-        Point3 target_point = new Point3(pos_takepic.getX() + ArucoModel.getPosX(),pos_takepic.getY() + ArucoModel.getPosY(),pos_takepic.getZ() + ArucoModel.getPosZ());
-        Log.d("AR",String.format("target point : %.3f %.3f %.3f",target_point.x,target_point.y,target_point.z));
+        Point target_point = new Point(pos_takepic.getX() + ArucoModel.getPosX(),pos_takepic.getY() + ArucoModel.getPosY(),pos_takepic.getZ() + ArucoModel.getPosZ());
+        Log.d("AR",String.format("target point : %.3f %.3f %.3f",target_point.getX(),target_point.getY(),target_point.getZ()));
 
+        Point target_relative = new Point(ArucoModel.getPosX(),ArucoModel.getPosY(),ArucoModel.getPosZ());
+        Quaternion rot_qua = alignX(target_relative);
+        Log.d("AR",String.format("rot qua : %f %f %f %f",rot_qua.getX(),rot_qua.getY(),rot_qua.getZ(),rot_qua.getW()));
 
-//        Point3 goal_target =  new Point3( QR_target.x + ArucoModel.getPosX(),QR_target.y  ,QR_target.z + ArucoModel.getPosZ());
-//        moveToWrapper( QRData.getPosX(),QRData.getPosY(),QRData.getPosZ(),0,0,-0.707,0.707);
-//        Point pos_target = new Point(pos_takepic.getX() + ArucoModel.getPosX(), pos_takepic.getY() + ArucoModel.getPosY(), pos_takepic.getZ() + ArucoModel.getPosZ());
-        Point3 laser_offset = new Point3(-0.0572,0,0.1111);
-        moveToWrapper(target_point.x + laser_offset.x,pos_takepic.getY(),target_point.z + laser_offset.z,0,0,-0.707,0.707);
-        Log.d("AR","Moved to target point");
-//        Log.d("QR", "move to A-");
+//        moveToWrapper(pos_takepic.getX(),pos_takepic.getY(),pos_takepic.getZ(),rot_qua.getX(),rot_qua.getY(),rot_qua.getZ(),rot_qua.getW());
+        relativeMoveToWrapper(0,0,0,rot_qua.getX(),rot_qua.getY(),rot_qua.getZ(),rot_qua.getW());
+        Log.d("AR","Aligned");
+        relativeMoveToWrapper(0,-0.0572 ,0.1111,rot_qua.getX(),rot_qua.getY(),rot_qua.getZ(),rot_qua.getW());
+        Log.d("AR","Offseted");
+
 
         api.laserControl(true);
         Log.d("QR", "laser");
@@ -139,12 +145,8 @@ public class YourService extends KiboRpcService {
         moveToWrapper(10.505,-8.5,4.50, 0,0, -0.707, 0.707); // avoid KOZ2
 
         moveToWrapper(10.6, -8.0, 4.5,0, 0, -0.707, 0.707); //move to Point B
-
-
-
         api.reportMissionCompletion();
     }
-
 
     @Override
     protected void runPlan2(){
@@ -154,17 +156,6 @@ public class YourService extends KiboRpcService {
     @Override
     protected void runPlan3(){
         // write here your plan 3
-    }
-
-    public String decodeQR(Mat qr_img)
-    {
-
-        zbarQR_obj = new zbarQR();
-        zbarQR_obj.scanQRImage(imgProc.sharpenImg);
-        Log.d("QR","readed : " + zbarQR_obj.qrCodeString);
-        api.sendDiscoveredQR(zbarQR_obj.qrCodeString);
-
-        return zbarQR_obj.qrCodeString;
     }
 
 
@@ -178,10 +169,44 @@ public class YourService extends KiboRpcService {
                 (float)qua_z, (float)qua_w);
         Result result = api.moveTo(point, quaternion, false);
         int loopCounter = 0;
-        while(!result.hasSucceeded()|| loopCounter < LOOP_MAX){
+        while(!result.hasSucceeded() && loopCounter < LOOP_MAX){
             result = api.moveTo(point, quaternion, false);
             ++loopCounter;
         }
+    }
+
+    public void relativeMoveToWrapper(double pos_x, double pos_y, double pos_z,
+                              double qua_x, double qua_y, double qua_z,
+                              double qua_w){
+        final int LOOP_MAX = 2;
+        final Point point = new Point(pos_x, pos_y, pos_z);
+
+        final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y,
+                (float)qua_z, (float)qua_w);
+        Result result = api.relativeMoveTo(point, quaternion, false);
+        int loopCounter = 0;
+        while(!result.hasSucceeded() && loopCounter < LOOP_MAX){
+            result = api.relativeMoveTo(point, quaternion, false);
+            ++loopCounter;
+        }
+    }
+
+    public Quaternion alignX(Point target)
+    {
+        double target_dist = Math.sqrt(target.getX() * target.getX() + target.getY() * target.getY() + target.getZ() * target.getZ());
+
+        Point v = new Point(target.getX()/target_dist,target.getY()/target_dist,target.getZ()/target_dist);
+        double rot_angle = Math.acos(v.getX());
+        double u_y = v.getZ() * - 1.0f; //i cross k
+        double u_z = v.getY(); //i cross j
+        Point u = new Point(0,u_y,u_z);
+
+        double qw = Math.cos(rot_angle/2);
+        double qx = Math.sin(rot_angle/2) * u.getX();
+        double qy = Math.sin(rot_angle/2) * u.getY();
+        double qz = Math.sin(rot_angle/2) * u.getZ();
+
+        return new Quaternion((float)qx,(float)qy,(float)qz,(float)qw);
     }
 
 }
