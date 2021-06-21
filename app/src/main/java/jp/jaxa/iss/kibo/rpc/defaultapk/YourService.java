1aa46592e634ab82860e6deab99249b469ac1adc
Tonieie <tonzazil12@gmail.com>
2021-06-21 14:31:52 +0700

    added relative2 and laser pos log

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -93,6 +93,12 @@ public class YourService extends KiboRpcService {
         pos_takepic = kinec_takepic.getPosition();
         Log.d("AR", String.format("After x,y,z : %f %f %f", pos_takepic.getX(), pos_takepic.getY(), pos_takepic.getZ()));
 
+        double target_relative2_x = 11.2161f - (pos_takepic.getX() + 0.0572);
+        double target_relative2_y = -10.585 - (pos_takepic.getY() - 0.1302);
+        double target_relative2_z = 5.38 - (pos_takepic.getZ() - 0.1111);
+        Point target_relative2 = new Point(target_relative2_x,target_relative2_y,target_relative2_z);
+        Log.d("AR", String.format("laser x,y,z : %f %f %f", pos_takepic.getX() + 0.0572, pos_takepic.getY() - 0.1302, pos_takepic.getZ() - 0.1111));
+        Log.d("AR", String.format("target_relative2 x,y,z : %f %f %f", target_relative2.getX(), target_relative2.getY(), target_relative2.getZ()));
 
 //        Kinematics kinec_takepic = api.getTrustedRobotKinematics();
 //        Point pos_takepic = kinec_takepic.getPosition();
@@ -108,7 +114,7 @@ public class YourService extends KiboRpcService {
 //        Point target_relative = new Point(ArucoModel.getPosX() + 0.0572,ArucoModel.getPosY() - 0.1302,ArucoModel.getPosZ() - 0.1111);   //offset from NavCam to LaserPointer
 //        Log.d("AR",String.format("target relative : %f %f %f",target_relative.getX(),target_relative.getY(),target_relative.getZ()));
 
-        Point target_relative2 = new Point(11.2161f - (pos_takepic.getX() + 0.0572),-10.585 - (pos_takepic.getY() - 0.1302),5.38 - (pos_takepic.getZ() - 0.1111));
+
         Quaternion rot_qua = alignX(target_relative2);
 
         Log.d("AR",String.format("rot qua : %f %f %f %f",rot_qua.getX(),rot_qua.getY(),rot_qua.getZ(),rot_qua.getW()));
